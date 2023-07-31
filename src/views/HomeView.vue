aaee4f71ed233b3a7d92148c7c16019717c51ced
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-31 18:03:14 +0100

    add store and recipe logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,65 @@
 <template>
   <div class="home">
     <h1>My Recipes</h1>
-    <button>Add new Recipe</button>
+    <button @click="togglePopup">Add new Recipe</button>
+
     <div class="recipes">
-      <!--Recipes to go here-->
+      <div
+        class="card"
+        v-for="recipe in $store.state.recipes"
+        :key="recipe.slug"
+      >
+        <h2>{{ recipe.title }}</h2>
+        <p>{{ recipe.description }}</p>
+        <router-link :to="`/recipe/${recipe.slug}`">
+          <button>View Recipe</button>
+        </router-link>
+      </div>
     </div>
 
-    <div class="add-recipe-popup">
+    <div class="add-recipe-popup" v-if="popupOpen">
       <div class="popup-content">
-        <h2>Add new Recipe</h2>
-        <form @submit.prevent="">
+        <h2>Add new recipe</h2>
+
+        <form @submit.prevent="addNewRecipe">
           <div class="group">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" v-model="newRecipe.title" />
           </div>
 
           <div class="group">
             <label>Description</label>
-            <textarea />
+            <textarea v-model="newRecipe.description"></textarea>
           </div>
 
           <div class="group">
             <label>Ingredients</label>
-            <div class="ingredient">
-              <input type="text" />
+            <div
+              class="ingredient"
+              v-for="i in newRecipe.ingredientRows"
+              :key="i"
+            >
+              <input type="text" v-model="newRecipe.ingredients[i - 1]" />
             </div>
-            <button>Add ingredient</button>
+            <button type="button" @click="addNewIngredient">
+              Add Ingredient
+            </button>
           </div>
 
           <div class="group">
             <label>Method</label>
-            <div class="method">
-              <textarea />
+            <div class="method" v-for="i in newRecipe.methodRows" :key="i">
+              <textarea v-model="newRecipe.method[i - 1]"></textarea>
             </div>
-            <button>Add step</button>
+            <button type="button" @click="addNewStep">Add step</button>
           </div>
 
-          <button type="submit">Add recipe</button>
-          <button>Close</button>
+          <div class="button-container">
+            <button type="submit">Add Recipe</button>
+            <button type="button" class="red-button" @click="togglePopup">
+              Close
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -46,8 +68,67 @@
 
 <script>
 import { ref } from "vue";
+import { useStore } from "vuex";
+
 export default {
-  name: "HomeView",
+  name: "home-page",
+  setup() {
+    const newRecipe = ref({
+      title: "",
+      description: "",
+      ingredients: [],
+      method: [],
+      ingredientRows: 1,
+      methodRows: 1,
+    });
+    const popupOpen = ref(false);
+    const store = useStore();
+
+    const togglePopup = () => {
+      popupOpen.value = !popupOpen.value;
+    };
+
+    const addNewIngredient = () => {
+      newRecipe.value.ingredientRows++;
+    };
+
+    const addNewStep = () => {
+      newRecipe.value.methodRows++;
+    };
+
+    const addNewRecipe = () => {
+      newRecipe.value.slug = newRecipe.value.title
+        .toLowerCase()
+        .replace(/\s/g, "-");
+
+      if (!newRecipe.value.slug) {
+        alert("Please enter a title");
+        return;
+      }
+
+      store.commit("ADD_RECIPE", { ...newRecipe.value });
+
+      newRecipe.value = {
+        title: "",
+        description: "",
+        ingredients: [],
+        method: [],
+        ingredientRows: 1,
+        methodRows: 1,
+      };
+
+      togglePopup();
+    };
+
+    return {
+      newRecipe,
+      addNewRecipe,
+      togglePopup,
+      popupOpen,
+      addNewStep,
+      addNewIngredient,
+    };
+  },
 };
 </script>
 
@@ -105,6 +186,8 @@ h1 {
   border-radius: 1rem;
   width: 100%;
   max-width: 768px;
+  max-height: 80vh;
+  overflow-y: auto;
 }
 
 .popup-content h2 {
@@ -138,5 +221,20 @@ h1 {
 
 .popup-content button[type="submit"] {
   margin-right: 1rem;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+
+.button-container button {
+  margin: 0 10px;
+  width: 150px;
+}
+
+.red-button {
+  background-color: #bb3636;
+  color: white;
 }
 </style>
